11e2c5182033a6f7ac490b03fc8867380f180641
Saifur Rahman <safrrhmn@hotmail.com>
2015-03-27 18:06:10 -0500

    Fixed exception type

--- src/PageObjects/PageObjectHelpers/BaseClass.java
+++ src/PageObjects/PageObjectHelpers/BaseClass.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.NoSuchElementException;
+
 /**
  * Defines the generic methods/functions for PageObjects.
  */
@@ -44,7 +46,7 @@ public class BaseClass {
 
             driver.findElement(by).isDisplayed();
 
-        } catch (NoSuchFieldError ex) {
+        } catch (NoSuchElementException ex) {
 
             throw ex;
         }
