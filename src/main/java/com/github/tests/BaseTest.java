4ec335e086c40d4bd1f4b007aeff54b0906abcb9
Saifur <safrrhmn@hotmail.com>
2015-06-10 23:02:14 -0600

    Clean up

--- src/main/java/com/github/tests/BaseTest.java
+++ src/main/java/com/github/tests/BaseTest.java
@@ -4,9 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Parameters;
+import org.testng.annotations.*;
 
 public class BaseTest {
 
